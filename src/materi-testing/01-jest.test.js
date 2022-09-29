eeefa5cd26e5e4bfe2c0bf94f63b8d1b10c07f51
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-09-29 17:21:08 +0700

    Perbedaan describe, test dan it pada file testing

--- src/materi-testing/01-jest.test.js
+++ src/materi-testing/01-jest.test.js
@@ -1,29 +1,36 @@
 import { penjumlahan, perkalian } from "./angka";
 import { potongPajak, sapaan } from "./text";
 
-test("testing sederhana", () => {
-  expect(true).toBe(true);
+describe("basic testing", () => {
+  it("testing sederhana", () => {
+    expect(true).toBe(true);
+  });
+
+  // it.only("menjumlahkan nilai", () => {
+  it("menjumlahkan nilai", () => {
+    expect(1 + 1).toBe(2);
+    expect(2 * 3).toBe(6);
+  });
 });
 
-test("menjumlahkan nilai", () => {
-  expect(1 + 1).toBe(2);
-  expect(2 * 3).toBe(6);
+describe("Group Testing Angka", () => {
+  test("Fungsi Penjumlahan", () => {
+    expect(penjumlahan(3, 2)).toBe(5);
+    expect(penjumlahan(4, 8)).toBe(12);
+  });
+
+  test("Fungsi Perkalian", () => {
+    expect(perkalian(3, 2)).toBe(6);
+    expect(perkalian(5, 3)).toBe(15);
+  });
 });
 
-test("Fungsi Penjumlahan", () => {
-  expect(penjumlahan(3, 2)).toBe(5);
-  expect(penjumlahan(4, 8)).toBe(12);
-});
+describe("Group Testing Text/String", () => {
+  test("Mengetahui gaji bersih di kurangi pajak", () => {
+    expect(potongPajak(10000000)).toBe(`Gaji bersih anda Rp${9000000}`);
+  });
 
-test("Fungsi Perkalian", () => {
-  expect(perkalian(3, 2)).toBe(6);
-  expect(perkalian(5, 3)).toBe(15);
-});
-
-test("Mengetahui gaji bersih di kurangi pajak", () => {
-  expect(potongPajak(10000000)).toBe(`Gaji bersih anda Rp${9000000}`);
-});
-
-test("Menyapa nama", () => {
-  expect(sapaan("Abu")).toBe("Hallo Abu");
+  test("Menyapa nama", () => {
+    expect(sapaan("Abu")).toBe("Hallo Abu");
+  });
 });
