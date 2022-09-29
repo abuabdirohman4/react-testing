f53c8614bf80efba6b8bc48d3b569e08862dd1a9
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-09-29 17:07:50 +0700

    6. Menguji sebuah fungsi

--- src/materi-testing/01-jest.test.js
+++ src/materi-testing/01-jest.test.js
@@ -1,3 +1,13 @@
+function penjumlahan(angka1, angka2) {
+  return angka1 + angka2;
+  //   return 5;
+  //   return angka1 * angka2;
+}
+
+function perkalian(angka1, angka2) {
+  return angka1 * angka2;
+}
+
 test("testing sederhana", () => {
   expect(true).toBe(true);
 });
@@ -5,4 +15,14 @@ test("testing sederhana", () => {
 test("menjumlahkan nilai", () => {
   expect(1 + 1).toBe(2);
   expect(2 * 3).toBe(6);
+});
+
+test("Fungsi Penjumlahan", () => {
+  expect(penjumlahan(3, 2)).toBe(5);
+  expect(penjumlahan(4, 8)).toBe(12);
+});
+
+test("Fungsi Perkalian", () => {
+  expect(perkalian(3, 2)).toBe(6);
+  expect(perkalian(5, 3)).toBe(15);
 });
