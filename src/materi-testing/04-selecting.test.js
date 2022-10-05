bca887ad7a173d7dc053378bea64e32df3e2eeae
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-10-05 10:56:23 +0700

    Selecting element untuk diberikan fireEvent

--- src/materi-testing/04-selecting.test.js
+++ src/materi-testing/04-selecting.test.js
@@ -7,10 +7,30 @@ describe("untuk testing select element dan dijadikan expect value", () => {
     const txt = screen.getByText("Saya Sedang Sembunyi");
     expect(txt).toBeInTheDocument();
   });
-  
+
   test("memastikan content text button sesuai saat pertama render", () => {
     render(<App />);
     const txt = screen.getByText("Munculkan");
     expect(txt).toBeInTheDocument();
   });
-})
+});
+
+describe("melakukan selecting element untuk diberikan trigger fireEvent", () => {
+  test("memastikan text yang dirender setelah di klik", () => {
+    render(<App />);
+    const btn = screen.getByText("Munculkan");
+    fireEvent.click(btn);
+
+    const txt = screen.getByText("Saya Sedang Muncul");
+    expect(txt).toBeInTheDocument();
+  });
+
+  test("Memasikan text di dalam button sesuai setelah button di click", () => {
+    render(<App />);
+    const btn = screen.getByText("Munculkan");
+    fireEvent.click(btn);
+
+    const btnResult = screen.getByText("Sembunyikan");
+    expect(btnResult).toBeInTheDocument();
+  });
+});
